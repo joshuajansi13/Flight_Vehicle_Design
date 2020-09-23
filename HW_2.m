0e251e5d46f2355958c8f2cca3404817b7ca3aa5
Joshua Canlas <canlas.joshua0121@gmail.com>
2020-09-23 15:09:52 -0600

    final code

--- HW_2.m
+++ HW_2.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 rho = 1.225;
-v_infinity = 3:0.5:20;
+v_infinity = 3:0.1:20;
 wing_span = 0.9144;
 tail_span = 0.1524;
 wing_chord = 0.20352;
@@ -26,7 +26,7 @@ e_inv = 0.98*(1-2*(fuselage_diameter/wing_span)^2);
 
 mu = 1.81*10^-5;
 
-exposed_area_of_wing = wing_span*wing_chord;
+exposed_area_of_wing = wing_span*wing_chord - (fuselage_diameter*wing_chord);
 exposed_area_of_tail = tail_span*tail_chord;
 
 wetted_areas_of_wing_tail_fuselage = [
@@ -36,20 +36,20 @@ wetted_areas_of_wing_tail_fuselage = [
     ];
 
 k_values_of_wing_tail_fuselage = [
-    1+2*cos(sweep_of_wing)*wing_thickness_to_chord_ratio+100*wing_thickness_to_chord_ratio^4;
-    1+2*cos(sweep_of_tail)*tail_thickness_to_chord_ratio+100*tail_thickness_to_chord_ratio^4;
+    1+2*cos(sweep_of_wing)*wing_thickness_to_chord_ratio+100*(wing_thickness_to_chord_ratio)^4;
+    1+2*cos(sweep_of_tail)*tail_thickness_to_chord_ratio+100*(tail_thickness_to_chord_ratio)^4;
     calculate_k_of_fuselage(fuselage_length, fuselage_diameter)
     ];
 
 lift_of_plane_in_newtons = 9.32;
 
-for index = 1:length(v_infinity)
-    dynamic_pressure = 0.5*rho*v_infinity(index)^2;
+for R = 1:length(v_infinity)
+    dynamic_pressure(R) = 0.5*rho*v_infinity(R)^2;
     
     reynolds_numbers_wing_tail_fuselage = [
-        rho*v_infinity(index)*wing_chord/mu;
-        rho*v_infinity(index)*tail_chord/mu;
-        rho*v_infinity(index)*fuselage_length/mu
+        (rho*v_infinity(R)*wing_chord)/mu;
+        (rho*v_infinity(R)*tail_chord)/mu;
+        (rho*v_infinity(R)*fuselage_length)/mu
         ];
     
     C_f_turbulent_wing_tail_fuselage = [
@@ -59,22 +59,22 @@ for index = 1:length(v_infinity)
         ];
     
     C_d_p_of_wing_tail_fuselage = [
-        k_values_of_wing_tail_fuselage(1)*C_f_turbulent_wing_tail_fuselage(1)*wetted_areas_of_wing_tail_fuselage(1)/reference_area;
-        k_values_of_wing_tail_fuselage(2)*C_f_turbulent_wing_tail_fuselage(2)*wetted_areas_of_wing_tail_fuselage(2)/reference_area;
-        k_values_of_wing_tail_fuselage(3)*C_f_turbulent_wing_tail_fuselage(3)*wetted_areas_of_wing_tail_fuselage(3)/reference_area
+        k_values_of_wing_tail_fuselage(1)*C_f_turbulent_wing_tail_fuselage(1)*(wetted_areas_of_wing_tail_fuselage(1)/reference_area);
+        k_values_of_wing_tail_fuselage(2)*C_f_turbulent_wing_tail_fuselage(2)*(wetted_areas_of_wing_tail_fuselage(2)/reference_area);
+        k_values_of_wing_tail_fuselage(3)*C_f_turbulent_wing_tail_fuselage(3)*(wetted_areas_of_wing_tail_fuselage(3)/reference_area)
         ];
     
     total_parasitic_drag_coefficient = C_d_p_of_wing_tail_fuselage(1)+C_d_p_of_wing_tail_fuselage(2)+C_d_p_of_wing_tail_fuselage(3);
     
-    e = 1 / ((1/e_inv)+K_constant*total_parasitic_drag_coefficient*pi*aspect_ratio);
+    e = 1 / ((1/e_inv)+(K_constant*total_parasitic_drag_coefficient*pi*aspect_ratio));
    
-    total_drag(index) = total_parasitic_drag_coefficient*dynamic_pressure*reference_area+lift_of_plane_in_newtons^2 / (dynamic_pressure*pi*wing_span^2*e);
+    total_drag = (total_parasitic_drag_coefficient*dynamic_pressure(R)*reference_area)+(lift_of_plane_in_newtons^2 / (dynamic_pressure(R)*pi*wing_span^2*e));
     
-    lift_to_drag_ratio(index) = lift_of_plane_in_newtons/total_drag(index);
+    lift_to_drag_ratio(R) = lift_of_plane_in_newtons/total_drag;
 end
 
-[lift_to_drag_ratio_max, index] = max(lift_to_drag_ratio);
-corresponding_v_infinity = v_infinity(index);
+[lift_to_drag_ratio_max, index] = max(lift_to_drag_ratio) % answer
+corresponding_v_infinity = v_infinity(index) % answer
 
 figure(1)
 plot(v_infinity, lift_to_drag_ratio, 'b-')
@@ -83,13 +83,8 @@ plot(corresponding_v_infinity, lift_to_drag_ratio_max, 'kx', 'MarkerSize', 10)
 xlabel('Velocity (m/s)')
 ylabel('Lift-to-Drag Ratio')
 
-% TODO: find design flight speed (speed that maximizes L/D)- v_infinity_max
-%       corresponding Re
-%       corresponding lift coefficient - corresponding_lift_coefficient
-%       max L/D - lift_to_drag_ratio_max
-
-corresponding_dynamic_pressure = 0.5*rho*v_infinity(index)^2;
-corresponding_lift_coefficient = lift_of_plane_in_newtons/(corresponding_dynamic_pressure*reference_area);
+corresponding_lift_coefficient = lift_of_plane_in_newtons/(dynamic_pressure(index)*exposed_area_of_wing) % answer
+corresponding_Re = rho*corresponding_v_infinity*wing_chord/mu % answer
 
 function [k_fuse] = calculate_k_of_fuselage(fuselage_length, fuselage_diameter)
     fineness_ratio = fuselage_length/fuselage_diameter;
@@ -101,4 +96,3 @@ function [k_fuse] = calculate_k_of_fuselage(fuselage_length, fuselage_diameter)
         fprintf("Error");
     end
 end
-
